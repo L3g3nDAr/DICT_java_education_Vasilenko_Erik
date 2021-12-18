4adb67a7adc945532e279ccaa184a9a40b4ad835
L3g3nDA <erik.vasilenko2002@gmail.com>
2021-12-18 18:19:23 +0200

    TicTacToe 1 stage

---
+++ src/TicTacToe/TicTacToe.java
@@ -0,0 +1,8 @@
+package TicTacToe;
+import java.util.Scanner;
+
+public class TicTacToe {
+    public static void main(String[] args) {
+        System.out.println("X O X \n"+"O X O\n"+"X X O");
+    }
+}
